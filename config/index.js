ccd949a0aceb322cf5e19bd88681d46988786833
Julius Niedworok <julius.niedworok@uk.ibm.com>
2017-09-27 21:56:19 +0100

    Moved settings to config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const path = require('path');
 const url = require("url");
 
@@ -7,6 +8,13 @@ module.exports = {
 	server: {
 		port: 3000
 	},
+	settings: [
+		{"bluetooth" : true},
+		{"bluetooth_pairable" : false},
+		{"mpd" : true},
+		{"airplay" : true},
+		{"auto_source" : true}
+	],
 	swagger: {
 		spec: path.join(__dirname, '..', 'api', 'swagger.yaml'),
 		router: {
--- controllers/settings.js
+++ controllers/settings.js
@@ -2,26 +2,23 @@
 const config = require("../config");
 const joinUrl = require("url-join");
 
-//TODO move in config
-var settings = [{"bluetooth" : true}, {"bluetooth_pairable" : false}, {"mpd" : true}, {"airplay" : true}, {"auto_source" : true}];
-
 exports.getAllSettings = (req, res) => {
 	res.setHeader('content-type', 'application/json');
-	res.json(settings);
+	res.json(config.settings);
 }
 
 exports.getSettingStatus = (req, res) => {
-	if (!settings[req.swagger.params.setting-id.value])
+	if (!config.settings[req.swagger.params.setting-id.value])
 		res.status(400).send("invalid setting id");
 	else
-		res.status(200).json(settings);
+		res.status(200).json(config.settings);
 }
 
 exports.setSettingStatus = (req, res) => {
-	if (!settings[req.swagger.params.setting-id.value])
+	if (!config.settings[req.swagger.params.setting-id.value])
 		res.status(400).send("invalid setting id");
 	else {
-		settings[req.swagger.params.setting-id.value] = req.swagger.params.setting-status.value.status;
+		config.settings[req.swagger.params.setting-id.value] = req.swagger.params.setting-status.value.status;
 		res.status(200).send("successful operation");
 	}
 }
